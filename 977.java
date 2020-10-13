1a3b7538a873f26d5a3e66747de43548286a321e
davidsqf <67531024+davidsqf@users.noreply.github.com>
2020-10-13 19:47:21 +0800

    Create 977.java

---
+++ 977.java
@@ -0,0 +1,11 @@
+// This code is written by davidsqf.
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int[] B = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i] * A[i];
+        }
+        Arrays.sort(B);
+        return B;
+    }
+}
